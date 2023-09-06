25abae2512f47eb59389be803e1f2743e2a0fdc3
Jyoti <jyoti.biswal@schbang.com>
2023-09-06 17:55:24 +0530

    fix -4

--- components/HeroBannerSlider/HeroBannerSlider.js
+++ components/HeroBannerSlider/HeroBannerSlider.js
@@ -18,20 +18,20 @@ let data = [
         image: "/images/banner_1.png",
         id: '1'
     },
-    // {
-    //     title: "Outdoor Allergies.",
-    //     subTitle: "Take the first step towards understanding and managing ",
-    //     buttonLink: "#",
-    //     image: "/images/banner_2.png",
-    //     id: '1'
-    // },
-    // {
-    //     title: "Outdoor Allergies.",
-    //     subTitle: "Take the first step towards understanding and managing ",
-    //     buttonLink: "#",
-    //     image: "/images/banner_3.png",
-    //     id: '1'
-    // },
+    {
+        title: "Outdoor Allergies.lorem",
+        subTitle: "lorem ipsum dolor ",
+        buttonLink: "#",
+        image: "/images/banner_1.png",
+        id: '1'
+    },
+    {
+        title: "Outdoor Allergies. test",
+        subTitle: "Take the first step towards understanding and managing lorem ipsumaaa ",
+        buttonLink: "#",
+        image: "/images/banner_1.png",
+        id: '1'
+    },
 ]
 
 const HeroBannerSlider = () => {
@@ -42,42 +42,44 @@ const HeroBannerSlider = () => {
         <>
             {
                 data && data.length > 0 &&
-                    <div className='relative'>
-                        <Swiper
-                            slidesPerView={1}
-                            modules={[Autoplay, EffectFade, Pagination]}
-                            pagination={{ clickable: false, el: "#home-swiper-custom-pagination" }}
-                            autoplay={{ delay: 2000 }}
-                            effect="fade"
-                        >
-                            {
-                                data.length > 0 &&
-                                data.map((item, index) =>
-                                    <SwiperSlide key={index}>
-                                        <div className={styles.heroBanner}>
-                                            <div className='relative h-full'>
-                                                <Image src={item.image} fill alt={item.title} />
-                                            </div>
-                                            <div className={styles.contentContainer}>
-                                                <div className={styles.contentWrapper}>
+                <div className='relative'>
+                    <Swiper
+                        slidesPerView={1}
+                        modules={[Autoplay, EffectFade, Pagination]}
+                        pagination={{ clickable: true, el: "#home-swiper-custom-pagination" }}
+                        // autoplay={{ delay: 2000 }}
+                        effect="fade"
+                    >
+                        {
+                            data.length > 0 &&
+                            data.map((item, index) =>
+                                <SwiperSlide key={index}>
+                                    <div className={styles.heroBanner}>
+                                        <div className='relative h-full'>
+                                            <Image src={item.image} fill alt={item.title} />
+                                        </div>
+                                        <div className={styles.contentContainer}>
+                                            <div className={styles.contentWrapper}>
+                                                <div>
                                                     <div className={styles.subTitle}>{item.subTitle}</div>
                                                     <div className={styles.title}>{item.title}</div>
-                                                    <div className={styles.button}>
-                                                        <Link href={item.buttonLink || "#"}>
-                                                            <button>know More</button>
-                                                        </Link>
-                                                    </div>
+                                                </div>
+                                                <div className={styles.button}>
+                                                    <Link href={item.buttonLink || "#"}>
+                                                        <button>know More</button>
+                                                    </Link>
                                                 </div>
                                             </div>
                                         </div>
-                                    </SwiperSlide>
-                                )
-                            }
-                        </Swiper>
-                        <div id='home-swiper-custom-pagination' />
-                    </div>
-                    // :
-                    // <div></div>
+                                    </div>
+                                </SwiperSlide>
+                            )
+                        }
+                    </Swiper>
+                    <div id='home-swiper-custom-pagination' />
+                </div>
+                // :
+                // <div></div>
             }
         </>
     )
